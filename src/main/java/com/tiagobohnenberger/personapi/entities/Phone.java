78e4e75becdfbf65520486a2ee83efcaf25e5beb
Tiago Bohnenberger <tiagoboh10@gmail.com>
2021-06-23 17:22:50 -0300

    Initial template created with basic packages and classes

---
+++ src/main/java/com/tiagobohnenberger/personapi/entities/Phone.java
@@ -0,0 +1,28 @@
+package com.tiagobohnenberger.personapi.entities;
+
+import com.tiagobohnenberger.personapi.enums.PhoneType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data // automatic insert getters and setters
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Phone {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private PhoneType type;
+
+    @Column(nullable = false)
+    private String number;
+}
